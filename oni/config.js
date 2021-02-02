d67a94923437f25859d37fef253841365a0295d2
Takuya Nakamura <guitarspirits3@gmail.com>
2021-02-02 11:46:29 +0900

    add oni config

---
+++ oni/config.js
@@ -0,0 +1,38 @@
+"use strict";
+exports.__esModule = true;
+exports.activate = function (oni) {
+    console.log("config activated");
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", function () { return console.log("Control+Enter was pressed"); });
+    oni.input.bind("<f8>", "markdown.togglePreview")
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+};
+exports.deactivate = function (oni) {
+    console.log("config deactivated");
+};
+exports.configuration = {
+    //add custom config here, such as
+    "ui.colorscheme": "hybrid",
+    "autoClosingPairs.enabled": true,
+    "oni.loadInitVim": true,
+    "oni.hideMenu": true,
+    "sidebar.enabled": false,
+    "statusbar.enabled": false,
+    "editor.fontSize": "12px",
+    //"editor.fontFamily": "Monaco",
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+    "sidebar.default.open": false,
+    "commandline.mode": false,
+    // markdown
+    "experimental.markdownPreview.enabled": true,
+    "experimental.markdownPreview.syntaxHighlights": true,
+    "experimental.markdownPreview.syntaxTheme": "atom-one-dark"
+};
